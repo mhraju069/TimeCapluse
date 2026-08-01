e13e94adf31f69e902743018dfb09c6d17097c46
Raju <mhraju069@gmail.com>
2026-08-01 03:57:42 -0700

    Add: nav bar

---
+++ Frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Navbar from './pages/nav/navbar';
+
+function App() {
+  return (
+    <div style={{ maxWidth: '100vw', background: '#1a1a1a' }}>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ Frontend/src/pages/nav/navbar.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StaggeredMenu } from './menu';
+
+const menuItems = [
+    { label: 'Home', ariaLabel: 'Go to home page', link: '/' },
+    { label: 'About', ariaLabel: 'Learn about us', link: '/about' },
+    { label: 'Services', ariaLabel: 'View our services', link: '/services' },
+    { label: 'Contact', ariaLabel: 'Get in touch', link: '/contact' }
+];
+
+const socialItems = [
+    { label: 'Twitter', link: 'https://twitter.com' },
+    { label: 'GitHub', link: 'https://github.com' },
+    { label: 'LinkedIn', link: 'https://linkedin.com' }
+];
+
+const Navbar = () => {
+    return (
+        <div style={{ height: '100vh', background: '#1a1a1a' }}>
+            <StaggeredMenu
+                position="right"
+                items={menuItems}
+                socialItems={socialItems}
+                displaySocials
+                displayItemNumbering={true}
+                menuButtonColor="#ffffff"
+                openMenuButtonColor="#fff"
+                changeMenuColorOnOpen={true}
+                colors={['#B497CF', '#5227FF']}
+                logoUrl="/path-to-your-logo.svg"
+                accentColor="#5227FF"
+                onMenuOpen={() => console.log('Menu opened')}
+                onMenuClose={() => console.log('Menu closed')}
+            />
+        </div>
+    );
+};
+
+export default Navbar;
